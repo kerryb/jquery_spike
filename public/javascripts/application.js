19c39a4574b4a0c0eb3dd22775c03bf53040e81c
Kerry Buckley <kerryjbuckley@gmail.com>
2008-03-04 13:01:02 +0000

    Canvas responds to dropped element.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,19 @@
 
 // Make jquery and respond_to play together:
 //   http://ozmm.org/posts/jquery_and_respond_to.html
-jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
+
+// This gets executed once the page has finished laoding
+$(function() {
+	$(".draggable").draggable({helper: 'clone'});
+	$("#canvas").droppable({
+		accept: ".new_element",
+		activeClass: 'droppable-active',
+		hoverClass: 'droppable-hover',
+		drop: function(ev, ui) {
+			$(this).append("<p>Dropped!</p>");
+		}
+	});
+});
