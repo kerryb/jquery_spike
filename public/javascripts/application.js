a742fd45ef35aac02a0db9759ec5877a52b77a75
Kerry Buckley <kerryjbuckley@gmail.com>
2008-03-04 09:17:53 +0000

    Added jquery.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Make jquery and respond_to play together:
+//   http://ozmm.org/posts/jquery_and_respond_to.html
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
+})
